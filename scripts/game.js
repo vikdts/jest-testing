498468ae1f0fec3dd9ac07e1957adfd61e6c7383
vikdts <>
2023-10-28 05:12:34 +0000

    Test playerMoves, choices are true and their ids correct

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,8 @@
 let game = {
     currentGame: [],
+    playerMoves: [],
     score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
 
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -21,4 +21,13 @@ describe("game object contains correct keys", () => {
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
     });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
