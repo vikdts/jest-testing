aa47c6023ca6c89ca1c07d334dfe5c3c55ce32e7
vikdts <>
2023-10-29 05:35:24 +0000

    Create lightsOn function, change circle color

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -51,3 +51,26 @@ describe("newGame works correctly", () => {
         expect(game.currentGame.length).toBe(1);
     });
 });
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain(game.currentGame[0] + "light");
+    });
+});
